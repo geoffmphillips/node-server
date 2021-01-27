82ad9cdda6e4e61980d371de5e1e02c1f2dc769e
geoffmphillips <geoff.phillips@hey.com>
2021-01-26 17:51:01 -0800

    Init dotenv

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
-import http from 'http';
+import * as http from 'http';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 import { requestHandler } from './server/request_handler';
 
 const PORT = process.env.PORT
 
 http.createServer(requestHandler).listen(PORT);
+console.log(`listening on port ${PORT}`);
