041662d5e6ad86fc208f143a9dd323dfaf08aaec
geoffmphillips <geoff.phillips@hey.com>
2021-02-15 18:37:35 -0800

    Implement error serving

--- src/request_handler.ts
+++ src/request_handler.ts
@@ -1,14 +1,28 @@
+import fs from 'fs';
+import path from 'path';
 import { URL } from 'url';
-import { resolvedPromise } from './utils/resolved_promise';
+import { resolvedNull } from './utils/resolved_null';
 
-type contextType = {};
+type authType = {
+  isForbidden: boolean,
+  isAuthorized: boolean,
+}
 type requestType = {
   headers?: any,
   url?: string,
+  method?: string,
 };
 type responseType = {
-  end?: (data?: string) => void
+  setHeader: (header: string, value: string) => void,
+  end: (data?: string) => void,
+  writeHead: (errocode: number, headers?: {}) => void,
+  write: (html: Buffer) => void,
 };
+type contextType = {
+  request: requestType,
+  response: responseType,
+  auth?: authType,
+}
 type middlewareType = (context: contextType) => contextType;
 type requestHandlerType = (request: requestType, reponse: responseType) => Promise<null>
 
@@ -30,34 +44,57 @@ const requestHandlerConstructor = (
         };
       }, {}) : {};
   
-      const requestUrl = new URL(`${process.env.BASE_URL}${request.url}`);
+      const requestUrl: URL = new URL(`${process.env.BASE_URL}${request.url}`);
+      console.log(requestUrl)
   
-      const context = createContext(dbProvider, sessionHandler, ...middleware)({ 
+      const context: contextType = createContext(dbProvider, sessionHandler, ...middleware)({ 
         request, 
         response, 
         cookies,
         requestUrl,
       });
-  
+
+      const matchingRoute = findMatchingRoute(requestUrl.pathname, routes);
       
-      if (routes[requestUrl.pathname] && routes[requestUrl.pathname][context.request.method]) {
+      if (matchingRoute && matchingRoute[context.request.method]) {
         // TO DO implement AUTH
         // if (context.auth.isAuthorized) {
         if (true) {
-          routes[requestUrl.pathname][context.request.method](context)
+          matchingRoute[context.request.method](context)
         } else {
-          routes[context.auth.isForbidden ? '403' : '401'](context);
+          serveError(context.auth.isForbidden ? 403 : 401, response);
         }
-      } else if (routes['404']) {
-        routes['404'].GET(context);
+      } else {
+        serveError(404, response);
       }
     } catch (error) {
-      routes['500'].GET();
+      serveError(500, response);
     }
 
     response.end();
-    return await resolvedPromise;
+    return await resolvedNull;
   }
+}
+
+const findMatchingRoute = (pathname: string, routes): string | undefined => {
+  if (!routes) return undefined;
+
+  return routes[pathname] ? 
+    routes[pathname]
+  : routes[pathname + '/'] ?
+    routes[pathname + '/']
+  : pathname[pathname.length - 1] === '/' && routes[pathname.slice(0, pathname.length - 1)] ?
+    routes[pathname.slice(0, pathname.length - 1)]
+  :
+    undefined;
+}
+
+const serveError = (errorCode: number, response: responseType): void => {
+  const html = fs.readFileSync(path.join(__dirname + `/views/${errorCode}.html`));
+
+  response.setHeader('Content-Type', 'text/html');
+  response.writeHead(errorCode);
+  response.write(html);
 }
 
 const createContext = (...middleware: middlewareType[]) => (initialContext) => middleware.reduce((accContext, fn) => fn(accContext), initialContext)
