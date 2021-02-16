eca240e6f09b9657270bc200b7888b69bdf7b4b0
geoffmphillips <geoff.phillips@hey.com>
2021-02-15 19:45:46 -0800

    Comment out unnecessary code

--- src/db/utils/index.js
+++ src/db/utils/index.js
@@ -1,25 +1,22 @@
 // import { DateTime, Duration } from "luxon";
-
 // export function parseDbTimeStampTZ(datetime?: string): DateTime | undefined {
 //   if (!datetime) {
 //     return undefined;
 //   }
-
 //   try {
 //     return DateTime.fromISO(datetime);
 //   } catch(error) {
 //     return undefined;
 //   }
 // }
-
 // export function parseDbMilliseconds(ms?: string): Duration | undefined {
 //   if (!ms) {
 //     return undefined;
 //   }
-
 //   try {
 //     return Duration.fromMillis(+ms);
 //   } catch(error) {
 //     return undefined;
 //   }
 // }
+//# sourceMappingURL=index.js.map
