8d34e4944949426b4f0592154de16f7e63ebe607
geoffmphillips <geoff.phillips@hey.com>
2021-02-15 18:41:54 -0800

    Rename

---
+++ src/db/db.ts
@@ -0,0 +1,27 @@
+import pgPromise from 'pg-promise';
+import { getSettings, settingsType } from '../config/settings';
+
+type Ext = {};
+export type dbType = pgPromise.IDatabase<Ext>;
+
+export function dbProviderCtor(settings: () => Promise<settingsType>, test: boolean = false) {
+  let db: any;
+
+  async function dbProvider<T>(callback: (db: dbType) => Promise<T>): Promise<T> {
+    if (!db) {
+      const { databaseUrl, databaseUrlTest } = await settings();
+      const pgPromiseOptions = {};
+
+      db = pgPromise<Ext>(pgPromiseOptions)(test ? databaseUrlTest : databaseUrl);
+    }
+
+    return db.tx((db: dbType) => {
+      return callback(db);
+    });
+  }
+
+  return dbProvider;
+}
+
+export const dbProvider = dbProviderCtor(getSettings);
+export type dbProviderType = typeof dbProvider;
