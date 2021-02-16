d02aa4ad651dc9a391aeb2f2026f9154999dd71a
geoffmphillips <geoff.phillips@hey.com>
2021-02-15 19:46:28 -0800

    Create seeds

---
+++ src/db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (id, email, digest, roles)
+VALUES
+  ('00000000-0000-0000-0000-000000000000', 'user@test.test', '', '{user}'),
+  ('00000000-0000-0000-0000-000000000001', 'admin@test.test', '', '{user, staff, admin}');
