2b77cb19404213c237f0d0d313788935b6d5903f
geoffmphillips <geoff.phillips@hey.com>
2021-02-15 18:06:00 -0800

    rename

---
+++ src/utils/resolved_null.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.resolvedNull = void 0;
+exports.resolvedNull = Promise.resolve(null);
+//# sourceMappingURL=resolved_null.js.map
