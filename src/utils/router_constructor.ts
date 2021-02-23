b44a6ee77985dde410aeb77c8779b34f4d9c393f
geoffmphillips <geoff.phillips@hey.com>
2021-02-22 21:53:57 -0800

    Add auth to router constructor

--- src/utils/router_constructor.ts
+++ src/utils/router_constructor.ts
@@ -1,26 +1,49 @@
+import { authType } from '../routes/auth/index';
+import { contextType } from '../request_handler';
+
 type controllerMethodType = (context) => Promise<any>
-type addRouteType = (path: string, controllerMethod: controllerMethodType, httpMethod: string, context: routerType) => void;
-type typedAddRoute = (path: string, method: controllerMethodType) => void;
+type addRouteType = (
+  path: string, 
+  controllerMethod: controllerMethodType, 
+  httpMethod: string,
+  auth: authType,
+) => void;
+type typedAddRoute = (path: string, method: controllerMethodType, auth) => void;
+type routeType = {
+  [httpMethod: string]: {
+    auth: authType,
+    handler: (context: contextType) => void,
+  }
+};
 type routerType = {
   addRoute: addRouteType,
   get: typedAddRoute,
   post: typedAddRoute,
 }
 
+
 function routerContstructor(prefix: string): routerType {
-  const addRoute: addRouteType = (path, controllerMethod, httpMethod, __context) => {
-    __context[prefix + path] = { [httpMethod]: controllerMethod };
+  let router: undefined | routerType;
+
+  const addRoute: addRouteType = (path, controllerMethod, httpMethod, auth) => {
+    router[prefix + path] = { 
+      [httpMethod]: {
+        auth,
+        handler: controllerMethod,
+      }
+    };
   }
 
-  const router: routerType = {
+  router = {
     addRoute,
-    get: (path, method) => {
-      addRoute(path, method, 'GET', router);
+    get: (path, method, auth) => {
+      addRoute(path, method, 'GET', auth);
     },
-    post: (path, method) => {
-      addRoute(path, method, 'POST', router);
+    post: (path, method, auth) => {
+      addRoute(path, method, 'POST', auth);
     },
   }
+
   return router;
 }
 
