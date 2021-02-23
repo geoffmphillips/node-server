4b654ebfa022b74ab8da39a62c88db09ab5eba0e
geoffmphillips <geoff.phillips@hey.com>
2021-02-22 21:54:48 -0800

    Test auth

--- src/utils/router_constructor.test.ts
+++ src/utils/router_constructor.test.ts
@@ -2,8 +2,10 @@ import { routerContstructor } from './router_constructor';
 
 test('creates a router', async () => {
     const subject = routerContstructor('/test');
-    subject.get('/', async function () { return await Promise.resolve(1) });
-    const result = await subject['/test/'].GET();
+    subject.get('/', async function () { return await Promise.resolve(1) }, (session) => true);
+    const authResult = subject['/test/']['GET'].auth();
+    const result = await subject['/test/']['GET'].handler();
 
     expect(result).toBe(1);
+    expect(authResult).toBe(true);
 });
